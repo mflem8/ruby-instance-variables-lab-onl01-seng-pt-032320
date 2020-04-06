6505e0b77d86ac570e4b61af806042bc21fe8c3f
Matt Fleming <matthewfleming8@gmail.com>
2020-04-06 14:49:58 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,6 @@
 class Dog 
   def name =(dog_name)
-    this_dogs_name = dog name 
+    @this_dogs_name = dog name 
   end
   
   def name 
@@ -11,4 +11,4 @@ end
 lassie = Dog.new 
 lassie.name = "Lassie"
 
-lassie.name
+puts lassie.name
